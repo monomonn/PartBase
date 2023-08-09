450ef9b259bf66a0e146a01c390a7b3ed0fa741a
Monnomon <solomon.twc@gmail.com>
2023-08-09 22:04:47 +1200

    first commit

---
+++ webapi/Components.cs
@@ -0,0 +1,144 @@
+﻿using System.Text.Json.Serialization;
+
+
+public class PCPart
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; } // Common property for all component types
+
+    [JsonPropertyName("price")]
+    public decimal? Price { get; set; } // Common property for all component types
+
+    // Add other common properties here
+
+    // Leave out properties that are specific to CPU and GPU
+}
+
+public class CPU : PCPart
+{
+    [JsonPropertyName("core_count")]
+    public int? CoreCount { get; set; }
+
+    [JsonPropertyName("core_clock")]
+    public double? CoreClock { get; set; }
+
+    [JsonPropertyName("boost_clock")]
+    public double? BoostClock { get; set; }
+
+    [JsonPropertyName("tdp")]
+    public int? Tdp { get; set; }
+
+    [JsonPropertyName("graphics")]
+    public string? Graphics { get; set; }
+
+    [JsonPropertyName("smt")]
+    public bool? Smt { get; set; }
+
+    [JsonPropertyName("socket")]
+    public string? Socket { get; set; }
+}
+
+public class GPU : PCPart
+{
+    [JsonPropertyName("chipset")]
+    public string? Chipset { get; set; }
+
+    [JsonPropertyName("memory")]
+    public double? Memory { get; set; }
+
+    [JsonPropertyName("core_clock")]
+    public double? CoreClock { get; set; }
+
+    [JsonPropertyName("boost_clock")]
+    public double? BoostClock { get; set; }
+
+    [JsonPropertyName("color")]
+    public string? Color { get; set; }
+
+    [JsonPropertyName("length")]
+    public int? Length { get; set; }
+}
+
+public class Motherboard : PCPart
+{
+    [JsonPropertyName("socket")]
+    public string? Socket { get; set; }
+
+    [JsonPropertyName("form_factor")]
+    public string? FormFactor { get; set; }
+
+    [JsonPropertyName("max_memory")]
+    public double? MaxMemory { get; set; }
+
+    [JsonPropertyName("memory_slots")]
+    public double? MemorySlots { get; set; }
+
+    [JsonPropertyName("color")]
+    public string? Color { get; set; }
+
+    [JsonPropertyName("ram")]
+    public string? Ram { get; set; }
+}
+
+public class PSU : PCPart
+{
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("efficiency")]
+    public string? Effeciency { get; set; }
+
+    [JsonPropertyName("wattage")]
+    public double? Wattage { get; set; }
+
+    [JsonPropertyName("modular")]
+    public object? Modular { get; set; }
+
+    [JsonPropertyName("color")]
+    public string? Color { get; set; }
+
+}
+public class Memory : PCPart
+{
+    [JsonPropertyName("speed")]
+    public int?[] Speed { get; set; } 
+
+    [JsonPropertyName("modules")]
+    public int[] Modules { get; set; } 
+
+    [JsonPropertyName("price_per_gb")]
+    public decimal? PricePerGB { get; set; } 
+
+    [JsonPropertyName("color")]
+    public string? Color { get; set; } 
+
+    [JsonPropertyName("first_word_latency")]
+    public decimal? FirstWordLatency { get; set; } 
+
+    [JsonPropertyName("cas_latency")]
+    public float? CasLatency { get; set; }
+
+    [JsonPropertyName("ram")]
+    public string? Ram { get; set; }
+}
+
+public class Case : PCPart
+{
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("color")]
+    public string? Color { get; set; }
+
+    [JsonPropertyName("psu")]
+    public double? PSU { get; set; }
+
+    [JsonPropertyName("side_panel")]
+    public string? SidePanel { get; set; }
+
+    [JsonPropertyName("external_525_bays")]
+    public int? External525Bays { get; set; }
+
+    [JsonPropertyName("internal_35_bays")]
+    public int? Internal35Bays { get; set; }
+}
---
+++ webapi/Controllers/PCPartsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PCPartsController : ControllerBase
+    {
+        [HttpGet("{component}")]
+        public IEnumerable<PCPart> Get(string component)
+        {
+            string fileName;
+            switch (component.ToLower())
+            {
+                case "cpu":
+                    fileName = "cpu.json";
+                    return DeserializeJson<CPU>(fileName);
+                case "gpu":
+                    fileName = "video-card.json";
+                    return DeserializeJson<GPU>(fileName);
+                case "motherboard":
+                    fileName = "motherboard.json";
+                    return DeserializeJson<Motherboard>(fileName);
+                case "psu":
+                    fileName = "power-supply.json";
+                    return DeserializeJson<PSU>(fileName);
+                case "memory":
+                    fileName = "memory.json";
+                    return DeserializeJson<Memory>(fileName);
+                case "case":
+                    fileName = "case.json";
+                    return DeserializeJson<Case>(fileName);
+                // Add cases for other component types here
+                default:
+                    return new List<PCPart>();
+            }
+        }
+
+        private IEnumerable<T> DeserializeJson<T>(string fileName) where T : PCPart
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "json", fileName);
+
+            if (!System.IO.File.Exists(filePath))
+            {
+                return new List<T>();
+            }
+
+            // Read the JSON data from the file
+            var jsonData = System.IO.File.ReadAllText(filePath);
+
+            // Deserialize the JSON data into a list of the appropriate objects
+            return JsonSerializer.Deserialize<List<T>>(jsonData);
+        }
+    }
+}
